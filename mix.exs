1e6e4cb1100772c1310da27388f9403507580780
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2023-12-15 23:13:21 -0300

    add package

--- mix.exs
+++ mix.exs
@@ -5,10 +5,16 @@ defmodule Finanx.MixProject do
     [
       app: :finanx,
       version: "0.1.0",
-      authors: ["Elton Fonseca"],
       elixir: "~> 1.15",
+      authors: ["Elton Fonseca"],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      name: "Finanx",
+      homepage_url: "https://github.com/eltonfonseca/finanx",
+      description: """
+      Crawler for financial market informations.
+      """
     ]
   end
 
@@ -23,6 +29,15 @@ defmodule Finanx.MixProject do
       {:ecto, "~> 3.11.1"},
       {:floki, "~> 0.35.0"},
       {:httpoison, "~> 2.2.1"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Elton Fonseca"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/eltonfonseca/finanx"}
     ]
   end
 end
