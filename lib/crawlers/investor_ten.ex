c0d08a3b7112f9e976fd83143f3c2af3c0e9b420
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2023-12-18 23:34:18 -0300

    add parse investor ten

--- lib/crawlers/investor_ten.ex
+++ lib/crawlers/investor_ten.ex
@@ -1,5 +1,31 @@
 defimpl Finanx.Crawler, for: Finanx.Hosts.InvestorTen do
-  def quotes(_host, _ticker) do
-    {:ok, nil}
+  alias Finanx.Client.Request
+
+  def quotes(host, ticker) do
+    case Request.get(host, format_ticker(ticker)) do
+      {:ok, html} ->
+        {:ok, build_ticker(html)}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp format_ticker(ticker) do
+    ticker_down = String.downcase(ticker)
+    "#{ticker_down}/"
+  end
+
+  defp build_ticker(html) do
+    {:ok, document} = Floki.parse_document(html)
+
+    document
+    |> Floki.find(".stockCurrentQuotation")
+    |> Floki.find(".real")
+    |> Floki.text()
+    |> String.trim()
+    |> String.replace("R$\n", "")
+    |> String.replace(",", ".")
+    |> String.to_float()
   end
 end
